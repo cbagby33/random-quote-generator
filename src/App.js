45f5b24fb79551b500a2cfbac1fbb2aac21033c9
Calvin Bagby <cbagby33@gmail.com>
2026-01-30 02:52:51 -0500

    added necessary Redux dependencies and quote box component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class QuoteBox extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      quote: '',
+      author: '',
+      color:''
+    }
+  }
+  render () {
+    return (
+      <div className="inner-container">
+        <div id="quote-box">
+          <div id="text"></div>
+          <div id="author"></div>
+          <div className="buttons">
+            <a id="tweet-quote"></a>
+            <button id="new-quote">New Quote</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteBox />
     </div>
   );
 }
